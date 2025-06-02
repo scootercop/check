b008363f0424ce1820ea8a58162c94251d26d780
Naval Rishi <7453097+scootercop@users.noreply.github.com>
2025-06-02 22:58:33 +0530

    clean up

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
 import {StatusBar, Text, View} from 'react-native';
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-import AppNavigator from './navigation/AppNavigator';
 
 class MainApp extends Component {
   render() {
